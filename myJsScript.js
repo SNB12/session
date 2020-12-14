0965caa73918ea144ccff7f2e7e93f98e4e9753e
SNB12 <syedanikhathbasharath@gmail.com>
2020-12-14 13:16:51 +0530

    My First Commit

---
+++ myJsScript.js
@@ -0,0 +1,34 @@
+//console.log("hello from html doc")
+//var name = prompt("whats your name again");
+//console.log(name)
+
+var today = 6;
+
+switch (today) {
+    case 0:
+        console.log("sunday")
+        break;
+        case 1:
+            console.log("monday")
+            break;
+            case 2:
+            console.log("thuesday")
+            break;
+            case 3:
+            console.log("wednesday")
+            break;
+            case 4:
+            console.log("thursday")
+            break;
+            case 5:
+            console.log("friday")
+            break;
+            case 6:
+            console.log("saturday")
+            break;
+default:
+    console.log("you have entered and invalid value")
+    break;
+}
+var day = prompt("whats day today");
+console.log(day)
